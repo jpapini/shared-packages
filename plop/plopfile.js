15898b1e11ff1d0f3590a9e5dbef8e6b8b996d17
Julien Papini <julien.papini@gmail.com>
2024-06-17 13:48:45 +0200

    feat: add typescript package type to plop

--- plop/plopfile.js
+++ plop/plopfile.js
@@ -18,21 +18,47 @@ export default function (plop) {
                 validate: (input) => input.trim() !== '' || 'Package name cannot be empty.',
                 filter: (input) => toKebabCase(input.trim().toLocaleLowerCase()),
             },
+            {
+                type: 'list',
+                name: 'packageType',
+                message: 'What type of package do you want to create?',
+                choices: [
+                    { name: 'CommonJS', value: 'cjs' },
+                    { name: 'TypeScript', value: 'ts' },
+                ],
+            },
         ],
 
         actions: (answers) => {
-            /** @type {import('plop'.ActionType[])} */
-            return [
-                {
-                    type: 'addMany',
-                    destination: `${ROOT_DIR}/packages/${answers.packageName}/`,
-                    base: `${TEMPLATE_DIR}/package-cjs/`,
-                    templateFiles: `${TEMPLATE_DIR}/package-cjs/**`,
-                    globOptions: { dot: true },
-                    skipIfExists: false,
-                    force: true,
-                },
-            ];
+            const type = answers.packageType;
+
+            if (type === 'cjs') {
+                /** @type {import('plop'.ActionType[])} */
+                return [
+                    {
+                        type: 'addMany',
+                        destination: `${ROOT_DIR}/packages/${answers.packageName}/`,
+                        base: `${TEMPLATE_DIR}/package-cjs/`,
+                        templateFiles: `${TEMPLATE_DIR}/package-cjs/**`,
+                        globOptions: { dot: true },
+                        skipIfExists: false,
+                        force: true,
+                    },
+                ];
+            } else if (type === 'ts') {
+                /** @type {import('plop'.ActionType[])} */
+                return [
+                    {
+                        type: 'addMany',
+                        destination: `${ROOT_DIR}/packages/${answers.packageName}/`,
+                        base: `${TEMPLATE_DIR}/package-ts/`,
+                        templateFiles: `${TEMPLATE_DIR}/package-ts/**`,
+                        globOptions: { dot: true },
+                        skipIfExists: false,
+                        force: true,
+                    },
+                ];
+            }
         },
     });
 }
